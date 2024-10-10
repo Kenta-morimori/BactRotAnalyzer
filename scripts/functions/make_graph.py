c4c67f1e58892e528d3db3877b58754be26bf319
Takemori Kenta <64821676+Kenta-morimori@users.noreply.github.com>
2024-10-10 15:40:11 +0900

    :bug: edit for Actions

--- scripts/functions/make_graph.py
+++ scripts/functions/make_graph.py
@@ -312,3 +312,36 @@ def plot_SD_list_fft(freq_list, Amp_list, day, flag_std):
     else:
         plt.savefig(f"{save_dir}/SD-time-series_FFT_all.png")
     plt.close(fig)
+
+
+def dev_plot_time_list(day):
+    sample_num, _, _ = param.get_config(day)
+    save_dir = f"{param.save_dir_bef}/{day}/dev"
+    os.makedirs(save_dir, exist_ok=True)
+
+    time_list = read_csv.get_timelist(day)
+    time_diff_list = [
+        [time_list[i][j] - time_list[i][j - 1] for j in range(1, len(time_list[i]))] for i in range(len(time_list))
+    ]
+
+    fig, axs = plt.subplots(5, 2 * sample_num // 5, figsize=(2 * fig_size_x, fig_size_y))
+    for i in range(sample_num):
+        row = i // 2
+        col = i % 2
+        # time list
+        axs[row, 2 * col].plot(time_list[i])
+        axs[row, 2 * col].grid(True)
+        axs[row, 2 * col].set_title(f"time list No.{i+1}", fontsize=font_size)
+        axs[row, 2 * col].set_xlabel("Data Num", fontsize=font_size)
+        axs[row, 2 * col].set_ylabel("Time [s]", fontsize=font_size)
+        axs[row, 2 * col].tick_params(axis="both", which="major", labelsize=font_size)
+        # time diff
+        axs[row, 2 * col + 1].plot(time_list[i][1:], time_diff_list[i])
+        axs[row, 2 * col + 1].grid(True)
+        axs[row, 2 * col + 1].set_title(f"time diff list No.{i+1}", fontsize=font_size)
+        axs[row, 2 * col + 1].set_xlabel("Time [s]", fontsize=font_size)
+        axs[row, 2 * col + 1].set_ylabel("Time diff [s]", fontsize=font_size)
+        axs[row, 2 * col + 1].tick_params(axis="both", which="major", labelsize=font_size)
+    plt.tight_layout()
+    plt.savefig(f"{save_dir}/time_list.png")
+    plt.close(fig)
